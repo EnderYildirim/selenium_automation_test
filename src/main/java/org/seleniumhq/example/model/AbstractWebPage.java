15a32060bdea3dbcdb560351bd2c70e231d7ac0c
Ender Yıldırım <enderyildirim@outlook.com>
2017-09-17 17:23:44 +0300

    @PostContruct added instead of PageBeanPostProcessor class.

--- src/main/java/org/seleniumhq/example/model/AbstractWebPage.java
+++ src/main/java/org/seleniumhq/example/model/AbstractWebPage.java
@@ -1,12 +1,20 @@
 package org.seleniumhq.example.model;
 
+import javax.annotation.PostConstruct;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public abstract class AbstractWebPage implements WebPage {
     
     @Autowired 
     protected WebDriver webDriver;
+    
+    @PostConstruct
+    public void initElements() {
+    	PageFactory.initElements(webDriver, this);
+	}
     
     public void closePage() {
         webDriver.close();
